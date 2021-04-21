96b708a1c46fc2ae3dbbde92d2f63db347912777
mikeallen5 <allenmichael108@gmail.com>
2021-04-21 23:02:54 +0100

    Update r53.tf

--- terraform/modules/static_hosting/r53.tf
+++ terraform/modules/static_hosting/r53.tf
@@ -5,8 +5,8 @@ resource "aws_route53_record" "website_cloudfront" {
   zone_id         = data.aws_route53_zone.website.zone_id
 
   alias {
-    name                   = "${aws_cloudfront_distribution.website.domain_name}"
-    zone_id                = "${aws_cloudfront_distribution.website.hosted_zone_id}"
+    name                   = aws_cloudfront_distribution.website.domain_name
+    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
     evaluate_target_health = false
   }
 }
